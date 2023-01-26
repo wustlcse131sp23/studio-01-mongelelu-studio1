036fad8aba89fd8d784c56c0aa6235cc6a014562
sebastianmonge <sebastianmonge@sebastians-mbp.dhcp.wustl.edu>
2023-01-26 13:57:14 -0600

    Finished Studio 1

---
+++ src/studio1/Average.java
@@ -0,0 +1,18 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class Average {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("The first of two integers to be averaged");
+		int n1 = in.nextInt();
+		int n2 = in.nextInt();
+		
+		double average = ((double) n1+n2) / 2;
+		System.out.println("Average = " + average);
+	}
+
+}
---
+++ src/studio1/LeapYear.java
@@ -0,0 +1,19 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Insert a year: ");
+		int year = in.nextInt();
+		boolean leapYear = (year % 4 ==0 && (year % 100 != 0 || year % 400 ==0)); 
+				
+		System.out.println(year + " is a leap year: " + leapYear);
+		
+
+	}
+
+}
